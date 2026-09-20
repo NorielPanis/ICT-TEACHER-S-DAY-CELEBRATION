599c121824c6ab55c7f1b03b8da14dc7f985b27c
ItzNoriel <norielp27@gmail.com>
2026-09-20 18:47:46 +0800

    new commit

--- script/index.js
+++ script/index.js
@@ -7,3 +7,14 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 });
+
+const scrollDown = document.getElementById("scroll-down");
+const arrow = document.getElementById("scroll-arrow");
+
+scrollDown.addEventListener("mouseenter", () => {
+  arrow.classList.add("bouncing");
+});
+
+scrollDown.addEventListener("mouseleave", () => {
+  arrow.classList.remove("bouncing");
+});
